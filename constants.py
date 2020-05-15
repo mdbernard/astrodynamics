971aca151fae96d052a832ffdf40b0edce9d207f
Mike Bernard <mike.bernard@uconn.edu>
2020-05-14 23:19:05 -0400

    add mu magnitude

--- constants.py
+++ constants.py
@@ -1,4 +1,9 @@
-G = 6.67408e-11  # (m**3/kg/s**2)  gravitational constant of the universe
-M_Earth = 5.972e24  # (kg) mass of Earth
+from thermostate import Q_
+
+
+G = Q_(6.67408e-11, 'm**3/kg/s**2')  # (m**3/kg/s**2)  gravitational constant of the universe
+M_Earth = Q_(5.972e24, 'kg')  # (kg) mass of Earth
 
 mu_Earth = G*M_Earth  # (m**3/s**2) gravitational parameter of Earth
+mu_Earth_mag = mu_Earth.to_base_units().magnitude
+
