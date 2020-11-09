107c6e3da56803eb4ad66db877d4f82225a47f2c
Mike Bernard <michael.bernard@uconn.edu>
2020-11-09 17:57:28 -0500

    create find_orbital_elements

--- orbit.py
+++ orbit.py
@@ -27,3 +27,44 @@ def calc_AP_dot_2(RAdot, i):
     given time rate of change of Right Ascension (RAdot) and
     inclination. '''
     return RAdot*(2.5*(np.sin(i))**2 - 2)/(np.cos(i))
+
+
+def find_orbital_elements(r_, v_, mu):
+    ''' Calculate all six orbital elements given a position and
+    velocity vector at some point in the orbit, and the gravitational
+    parameter of the primary body. A variable with a trailing
+    underscore denotes a vector: x_ is a vector, x is the magnitude
+    of x_. Applies Algorithm 4.2 from Orbital Mechanics for Engineering
+    Students, 4 ed, Curtis. Returns angles in radians. '''
+    r = np.linalg.norm(r_)
+    v = np.linalg.norm(v_)
+    vr = np.dot(r, v)/r
+
+    # Specific Angular Momentum
+    h_ = np.cross(r, v)
+    h = np.linalg.norm(h_)
+
+    # Inclination
+    i = np.arccos(h_[2]/h)
+
+    # Right Ascension
+    Khat_ = np.array([0.0, 0.0, 1.0])
+    N_ = np.cross(Khat_, h_)
+    N = np.linalg.norm(N_)
+
+    RA = np.arccos(N_[2/N])
+    RA = RA if N_[1] >= 0 else 2*np.pi - RA
+
+    # Eccentricity
+    e_ = (1/mu)*(np.cross(v_, h_) - mu*r_/r)
+    e = np.linalg.norm(e_)
+
+    # Argument of Periapsis
+    AP = np.arccos(np.dot(N_/N, e_/e))
+    AP = AP if e_[2] >= 0 else 2*np.pi - AP
+
+    # True Anomaly
+    TA = np.arccos(np.dot(e_/e, r_/r))
+    TA = TA if vr >= 0 else 2*np.pi - TA
+
+    return h, i, RA, e, AP, TA
