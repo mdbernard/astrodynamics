13c9049b4b7a5813931decf341f966acd4abe280
Mike Bernard <mike.bernard@uconn.edu>
2020-11-09 15:27:18 -0500

    push buggy code

--- kepler.py
+++ kepler.py
@@ -114,15 +114,27 @@ def solve_kepler(r_0, v_0, dt, mu=398600, method='laguerre', tol=1e-7, max_iters
     :param v_0: `iterable` (km/s) initial velocity 3-vector
     :param dt: `float` (s) time after initial state to sovle for r, v
     :param mu: `float` (km**3/s**2) gravitational parameter, default Earth
+    :param method: `str` (--) which numerical method to use to solve Kepler's Equation
+    :param tol: `float` (--) decimal tolerance for numerical method (default 1e-7 is IEEE 745 single precision)
+    :param max_iters: `int` (--) maximum number of iterations in numerical method before breaking
     :return: (km) final position 3-vector, (km/s) final velocity 3-vector
     '''
+    VALID_METHODS = ('laguerre', 'newton')
+
     r0 = np.linalg.norm(r_0)  # (km) initial position magnitude
     v0 = np.linalg.norm(v_0)  # (km/s) initial velocity magnitude
     vr0 = np.dot(v_0, r_0)/r0  # (km/s) initial radial velocity magnitude
     alpha = 2/r0 - v0**2/mu  # (1/km) inverse of semi-major axis
 
     chi0 = np.sqrt(mu)*np.abs(alpha)*dt
-    chi, iters, converged = solve_kepler_laguerre(chi0, r0, vr0, mu, dt, alpha)
+
+    if method not in VALID_METHODS:
+        print(f'Method {method} is not valid, must be one of {VALID_METHODS}.\nDefaulting to laguerre method.')
+        chi, iters, converged = solve_kepler_laguerre(chi0, r0, vr0, mu, dt, alpha)
+    elif method == 'newton':
+        chi, iters, converged = solve_kepler_newton(chi0, r0, vr0, mu, dt, alpha)
+    else:  # method == 'laguerre'
+        chi, iters, converged = solve_kepler_laguerre(chi0, r0, vr0, mu, dt, alpha)
 
     f = calc_f(chi, r0, alpha)
     g = calc_g(dt, mu, chi, alpha)
@@ -143,50 +155,26 @@ def test():
     '''
     # given starting information
     mu = 398600  # (km**3/s**2) gravitational parameter, Earth
-    r_0_ = np.array([20000, -105000, -19000])  # (km) initial position vector
-    v_0_ = np.array([0.9, -3.4, -1.5])  # (km/s) initial velocity vector
+    r_0 = np.array([20000, -105000, -19000])  # (km) initial position vector
+    v_0 = np.array([0.9, -3.4, -1.5])  # (km/s) initial velocity vector
     dt = 24*60*60  # (s) time of interest after initial time
 
-    # given correct answer
-    correct_r_ = np.array([26338, -128750, -29656])  # (km) final position vector
-    correct_v_ = np.array([0.86280, -3.2116, -1.4613])  # (km/s) final velocity vector
+    # given correct answer from textbook
+    correct_r_1 = np.array([26338, -128750, -29656])  # (km) final position vector
+    correct_v_1 = np.array([0.86280, -3.2116, -1.4613])  # (km/s) final velocity vector
 
-    # applying Algorithm 3.4 (see reference textbook)
-    r_0 = np.linalg.norm(r_0_)
-    v_0 = np.linalg.norm(v_0_)
-    v_r_0 = np.dot(v_0_, r_0_)/r_0
-    alpha = 2/r_0 - v_0**2/mu
-
-    # set up numerical methods
-    chi_i = np.sqrt(mu)*np.abs(alpha)*dt
-    chi_n, iters_n, conv_n = solve_kepler_newton(chi_i, r_0, v_r_0, mu, dt, alpha)
-    chi_l, iters_l, conv_l = solve_kepler_laguerre(chi_i, r_0, v_r_0, mu, dt, alpha)
-
-    # solve with Newton
-    f_n = 1 - (chi_n**2/r_0)*C(alpha*chi_n**2)
-    g_n = dt - (1/np.sqrt(mu))*chi_n**3*S(alpha*chi_n**2)
-    r_n_ = f_n*r_0_ + g_n*v_0_
-    r_n = np.linalg.norm(r_n_)
-    fd_n = (np.sqrt(mu)/r_n/r_0)*(alpha*chi_n**3*S(alpha*chi_n**2) - chi_n)
-    gd_n =  1 - (chi_n**2/r_n)*C(alpha*chi_n**2)
-    v_n_ = fd_n*r_0_ + gd_n*v_0_
-
-    # solve with Laguerre
-    f_l = 1 - (chi_l**2/r_0)*C(alpha*chi_l**2)
-    g_l = dt - (1/np.sqrt(mu))*chi_l**3*S(alpha*chi_l**2)
-    r_l_ = f_l*r_0_ + g_l*v_0_
-    r_l = np.linalg.norm(r_l_)
-    fd_l = (np.sqrt(mu)/r_l/r_0)*(alpha*chi_l**3*S(alpha*chi_l**2) - chi_l)
-    gd_l =  1 - (chi_l**2/r_l)*C(alpha*chi_l**2)
-    v_l_ = fd_l*r_0_ + gd_l*v_0_
+    # solve using above methods
+    r_n, v_n = solve_kepler(r_0, v_0, dt, method='newton')
+    r_l, v_l = solve_kepler(r_0, v_0, dt, method='laguerre')
+    print(r_l, r_n)
 
     # check correctness
     # tolerance based on significant figures of given answers
-    newton_valid = np.allclose(r_n_, correct_r_, atol=1) and np.allclose(v_n_, correct_v_, atol=1e-4)
-    laguerre_valid = np.allclose(r_l_, correct_r_, atol=1) and np.allclose(v_l_, correct_v_, atol=1e-4)
+    newton_valid = np.allclose(r_n, correct_r_1, atol=1) and np.allclose(v_n, correct_v_1, atol=1e-4)
+    laguerre_valid = np.allclose(r_l, correct_r_1, atol=1) and np.allclose(v_l, correct_v_1, atol=1e-4)
 
-    return iters_n, iters_l
+    return all([newton_valid, laguerre_valid])
 
 
 if __name__ == '__main__':
-    test()
+    print(test())
--- lagrange.py
+++ lagrange.py
@@ -2,18 +2,17 @@ import numpy as np
 from stumpff import C, S
 
 
-def calc_f(chi, r_0, alpha):
-    return 1 - chi**2/r_0*C(alpha*chi**2)
+def calc_f(chi, r0, alpha):
+    return 1 - (chi**2/r0)*C(alpha*chi**2)
 
 
 def calc_g(dt, mu, chi, alpha):
-    return dt - 1/np.sqrt(mu)*chi**3*S(alpha*chi**2)
+    return dt - (1/np.sqrt(mu))*chi**3*S(alpha*chi**2)
 
 
-def calc_fd(mu, r, r_0, alpha, chi):
-    return np.sqrt(mu)/r/r_0*(alpha*chi**3*S(alpha*chi**2) - chi)
+def calc_fd(mu, r1, r0, alpha, chi):
+    return (np.sqrt(mu)/r1/r0)*(alpha*chi**3*S(alpha*chi**2) - chi)
 
 
-def calc_gd(chi, r, alpha):
-    return 1 - chi**2/r*C(alpha*chi**2)
-
+def calc_gd(chi, r1, alpha):
+    return 1 - (chi**2/r1)*C(alpha*chi**2)
