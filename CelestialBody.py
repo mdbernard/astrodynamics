c06c19565f0592bdc20cb04b410a2d7d6017b976
Mike Bernard <michael.bernard@uconn.edu>
2020-11-09 21:31:50 -0500

    add rotational_rate to CelestialBody

--- CelestialBody.py
+++ CelestialBody.py
@@ -1,3 +1,4 @@
+import numpy as np
 from constants import G
 
 
@@ -14,8 +15,18 @@ class CelestialBody:
         self.M = self.mass
         self.R = self.radius_equatorial
 
+        self.rotational_rate = None
+        self.w = None
+
+    def set_rotation_rate(self, rate):
+        ''' Set rotational rate about axis. '''
+        self.rotational_rate = rate  # (rad/s)
+        self.w = self.rotational_rate
+
 
 bodies = {
     'Earth': CelestialBody('Earth', 5.974e24, 6378, 0.003353, 1.08263e-3),
     'Moon': CelestialBody('Moon', 73.48e21, 1737, 0.0012, 202.7e-6)
 }
+
+bodies['Earth'].set_rotation_rate(7.292115e-5)  # (rad/s) Earth rotational velocity
