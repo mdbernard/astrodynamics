28ff673c098eff54d8853cda5042a88836f2a0d2
Mike Bernard <michael.bernard@uconn.edu>
2021-04-24 20:01:09 -0400

    add some more constants

--- constants.py
+++ constants.py
@@ -1,1 +1,6 @@
 G = 6.67408e-20  # (km**3/kg/s**2) gravitational constant of the universe
+g0 = 9.80665  # (m/s**2) standard gravity, Earth surface gravitational acceleration
+
+SIDEREAL_YEAR_TO_SIDEREAL_DAY = 365.256  # (day/year) conversion factor from sidereal years to sidereal days
+IEEE754_SINGLE = 1e-7
+IEEE754_DOUBLE = 1e-15
