bebb594997f91f47431c97d68de4c5b585ac87d4
Mike Bernard <mdbernard@protonmail.com>
2022-02-06 17:14:27 -0800

    fix *values -> array in __init__

--- attitude/parameter/base.py
+++ attitude/parameter/base.py
@@ -6,7 +6,7 @@ import numpy as np
 class BaseAttitudeParameter:
     """Base class for attitude parameterizations."""
 
-    def __init__(self, *values: np.ndarray) -> None:
+    def __init__(self, array: np.ndarray) -> None:
         """
         Abstract initialization based on numpy array of values.
         Derived classes should define how to initialize based on
